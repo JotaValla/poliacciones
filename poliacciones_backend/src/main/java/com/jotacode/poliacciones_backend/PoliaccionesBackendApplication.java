a15323d47cee1bb668d3e33313771e1669cb32d9
JotaValla <jimmy.valladares@epn.edu.ec>
2024-11-30 07:51:54 -0500

    chore: carga inicial del proyecto

---
+++ poliacciones_backend/src/main/java/com/jotacode/poliacciones_backend/PoliaccionesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jotacode.poliacciones_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliaccionesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoliaccionesBackendApplication.class, args);
+    }
+
+}
